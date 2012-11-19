8395d7d028f2081561aae292c8a6f81f83af563c
Stefan Sommer <stefan@nefan.dk>
2012-11-19 22:59:17 +0100

    README and (some) documentation

--- examples/mexfile.cpp
+++ examples/mexfile.cpp
@@ -2,7 +2,7 @@
  * This file is part of cljmex.
  *
  * Copyright (C) 2012, Stefan Sommer (sommer@diku.dk)
- * https://github.com/nefan/cljmex.git
+ * https://github.com/nefan/cljmex
  *
  * cljmex is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
